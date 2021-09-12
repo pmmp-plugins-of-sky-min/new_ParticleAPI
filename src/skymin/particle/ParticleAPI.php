efde360b8f98ee9baa46629a1e55268d639baf3c
skymin <ppt00003333@gmail.com>
2021-09-13 00:11:03 +0900

    update

--- src/skymin/particle/ParticleAPI.php
+++ src/skymin/particle/ParticleAPI.php
@@ -15,37 +15,24 @@ use function rad2deg;
 use function sin;
 use function sqrt;
 
-function circle(int $ParticleId, Vector3 $center, float $radius, float $unit, int $color, int $direction, float $start, float $finish, float $slope) :array{
-  if($start > $finish) $finish += 360;
+function circle(int $ParticleId, Vector3 $center, float $radius, float $unit, int $color, float $start, float $finish, float $slope) :array{
+  if($start >= $finish) $finish += 360;
 	$packets = array();
 	$x = $center->x;
 	$y = $center->y;
 	$z = $center->Z;
-	if($direction === 0){
-		for ($i = 0; $i < 360; $i += $unit){
+	for ($i = 0; $i < 360; $i += $unit){
 		if(($angle = $i + $start) > $finish) break;
 		$vec = new Vector3($x + sin(deg2rad($angle)) * $radius, $y + (sin(deg2rad($i)) * $slope), $z + cos(deg2rad($angle)) * $radius);
 		$packets[] = LevelEventPacket::standardParticle($ParticleId, $color, $vec);
-	  }
-		return $packets;
 	}
-	if($direction === 1){
-		for ($i = 0; $i < 360; $i += $unit){
-			if(($angle = 360 - $i + $start) > $finish) break;
-			$vec = new Vector3($x + sin(deg2rad($angle)) * $radius, $y + (sin(deg2rad($i)) * $slope), $z + cos(deg2rad($angle)) * $radius);
-			$packets[] = LevelEventPacket::standardParticle($ParticleId, $color, $vec);
-		}
-		return $packets;
-	}
+	return $packets;
 }
 
 class ParticleAPI{
 	
-	public const RIGHT = 0;
-	public const LEFT = 1;
-	
-	public static function drawCircle(int $ParticleId, Vector3 $center, float $radius, float $unit,  array $players, int $color = 0, int $direction = self::RIGHT, float $start = 0, float $finish = 360, float $slope = 0) :void{
-		$packets = circle($ParticleId, $center, $radius, $unit, $color, $direction, $start, $finish, $slope);
+	public static function drawCircle(int $ParticleId, Vector3 $center, float $radius, float $unit,  array $players, int $color = 0, float $start = 0, float $finish = 360, float $slope = 0) :void{
+		$packets = circle($ParticleId, $center, $radius, $unit, $color, $start, $finish, $slope);
 		Server::getInstance()->broadcastPackets($players, $packets);
 	}
 	
