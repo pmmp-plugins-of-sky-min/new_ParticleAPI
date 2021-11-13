55f490271f2b03b707654257d62cb6a43a88d724
skymin <ppt00003333@gmail.com>
2021-11-14 00:54:28 +0900

    example

---
+++ example/test2.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @name regulartest
+ * @main test\regulartest
+ * @author skymin
+ * @version SKY
+ * @api 4.0.0
+ */
+declare(strict_types = 1);
+namespace test;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+use pocketmine\math\Vector3;
+
+use pocketmine\color\Color;
+use pocketmine\network\mcpe\protocol\types\ParticleIds;
+
+use skymin\particle\ParticleAPI;
+
+class regulartest extends PluginBase implements Listener{
+	
+	public function onEnable() :void{
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+	
+	public function onTouch(PlayerInteractEvent $ev) :void{
+		$player = $ev->getPlayer();
+		$color = new Color(61,203,239);
+		$color = $color->toARGB();
+		ParticleAPI::drawRegular(ParticleIds::DUST, $player->getPosition()->add(0, 1.0, 0), 1.7, 3, 0.3, 0, [$player], $color);
+	}
+	
+}
